77a3e0e4f0d477f86307f984741d591a8d4fda8d
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-01-27 10:21:57 +1100

    SynthReason v0.71 *ULTRA*

--- SynthReason.py
+++ SynthReason.py
@@ -1,13 +1,13 @@
-# SynthReason v0.6 *ULTRA*
+# SynthReason v0.71 *ULTRA*
 # Copyright 2024 George Wagenknecht
 import re
 import random
 from collections import defaultdict
-size=250
-class Graph:
+size = 250
+recursion = 5
+class TextGraph:
     def __init__(self):
         self.graph = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-        self.aliases = {}
     def add_edge(self, u, v, w):
         weight = 1 
         self.graph[u][v][w] += weight
@@ -17,48 +17,33 @@ class Graph:
         current_word = start_word
         generated_text = [current_word]
         text_length = 1
-        position = 0
         while text_length < size:
             next_nodes = self.graph[current_word]
             if not next_nodes:
                 break
-            next_words = []
-            weights = []
-            total_weights = {}
+            next_words, weights = [], []
             for node, edges in next_nodes.items():
                 total_weight = sum(edges.values())
-                total_weights[node] = total_weight
-                next_nodes = self.graph[node]
-            if sum(total_weights.values()) <= 0:
-                break
-            for node, total_weight in total_weights.items():
                 if total_weight > 0:
                     next_words.append(node)
                     weights.append(total_weight)
-                    position += 1
+            if not next_words:
+                break
             next_word = random.choices(next_words, weights=weights, k=1)[0]
             generated_text.append(next_word)
             current_word = next_word
             text_length += 1
         return ' '.join(generated_text)
-def preprocess_text(text, user_words):
-    sentences = re.split(r'(?<=[.!?])\s+', text.lower())
-    user_words_set = set(user_words)
-    filtered_words = [word for sentence in sentences for word in sentence.split() if user_words_set.intersection(sentence.split())]
-    return filtered_words
-def create_word_graph(text, user_words, n=3):
-    words = text.split()
-    word_graph = Graph()
-    aliases = {}
-    for i in range(len(words) - n + 1):
-        ngram = tuple(words[i:i + n])
-        if ngram in aliases:
-            word_graph.add_edge(*aliases[ngram])
-        else:
-            aliases[ngram] = words[i:i + n]
-            word_graph.add_edge(*ngram)
-    return word_graph
-
+    def preprocess_text(self, text, user_words):
+        sentences = re.split(r'(?<=[.!?])\s+', text.lower())
+        user_words_set = set(user_words)
+        return [word for sentence in sentences for word in sentence.split() if user_words_set.intersection(sentence.split())]
+    def create_from_text(self, text, n=3):
+        words = text.split()
+        for i in range(len(words) - n + 1):
+            ngram = tuple(words[i:i + n])
+            self.add_edge(*ngram)
+text_graph = TextGraph()
 with open("FileList.conf", encoding="ISO-8859-1") as f:
     files = f.read().splitlines()
 with open("questions.conf", encoding="ISO-8859-1") as f:
@@ -74,11 +59,13 @@ for question in questions:
         if not user_input:
             continue
         user_words = re.sub("\W+", " ", user_input).split()
-        filtered_text = ' '.join(preprocess_text(text, user_words))
-        word_graph = create_word_graph(text, filtered_text)
-        generated_text = word_graph.generate_text(user_words[0])
-       
-
+        filtered_text = ' '.join(text_graph.preprocess_text(text, user_words))
+        text_graph.create_from_text(filtered_text)
+        generated_text = text_graph.generate_text(user_words[0])
+        for i in range(recursion):
+            filtered_text = ' '.join(text_graph.preprocess_text(generated_text, user_words))
+            text_graph.create_from_text(filtered_text)
+            generated_text = text_graph.generate_text(user_words[0])
         if generated_text:
             print("\nUsing:", file.strip(), "Answering:", user_input, "\nAI:", generated_text, "\n\n")
             with open(filename, "a", encoding="utf8") as f:
