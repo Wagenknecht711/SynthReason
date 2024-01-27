bf169b4b65a600c312899e68aa549dc96fab077a
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-01-27 20:37:31 +1100

    Add files via upload

--- SynthReason interactive.py
+++ SynthReason interactive.py
@@ -9,12 +9,7 @@ size = 250
 class TextGraph:
     def __init__(self):
         self.graph = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-    def add_edge(self, u, v, w, questions):
-        weight = 1
-        for question in questions:
-            if u in question.split() or v in question.split() or w in question.split():
-                weight += 1 
-        self.graph[u][v][w] += weight
+    
     def generate_text(self, start_word):
         if start_word not in self.graph:
             return "Start word not found."
@@ -42,33 +37,13 @@ class TextGraph:
         sentences = re.split(r'(?<=[.!?])\s+', text.lower())
         user_words_set = set(user_words)
         return [word for sentence in sentences for word in sentence.split() if user_words_set.intersection(sentence.split())]
-    def create_from_text(self, text, questions, n=3):
-        words = text.split()
-        for i in range(len(words) - n + 1):
-            ngram = tuple(words[i:i + n])
-            self.add_edge(*ngram, questions)
-        print("*")
-    def save_graph(self, filename):
-        current_graph_dict = {u: {v: dict(w_dict) for v, w_dict in v_dict.items()} for u, v_dict in self.graph.items()}
-        if os.path.exists(filename) and os.path.getsize(filename) > 0:
-            with open(filename, 'r') as f:
-                existing_data = json.load(f)
-            for u, v_dict in current_graph_dict.items():
-                for v, w_dict in v_dict.items():
-                    for w, weight in w_dict.items():
-                        existing_data.setdefault(u, {}).setdefault(v, {}).setdefault(w, 0)
-                        existing_data[u][v][w] += weight
-        else:
-            existing_data = current_graph_dict
-        with open(filename, 'w') as f:
-            json.dump(existing_data, f)
     def load_graph(self, filename):
         with open(filename, 'r') as f:
             regular_dict = json.load(f)
         self.graph = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
         for u, v_dict in regular_dict.items():
             for v, w_dict in v_dict.items():
-                for w, weight in w_dict.items():
+                for w, weight in reversed(w_dict.items()):
                     self.graph[u][v][w] = weight
 text_graph = TextGraph()
 with open("FileList.conf", encoding="ISO-8859-1") as f:
