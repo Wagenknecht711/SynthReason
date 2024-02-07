b310c5076f21a329176988cb83736cc2db3c7b77
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-02-07 17:08:49 +1100

    SynthReason v1.4 *ULTRA*

--- SynthReason.py
+++ SynthReason.py
@@ -1,45 +1,79 @@
-# SynthReason v1.31 *ULTRA*
+# SynthReason v1.4 *ULTRA*
 # Copyright 2024 George Wagenknecht
-import random
 import re
+import random
 import math
-token = "."
+from collections import defaultdict
 size = 250
-with open("fileList.conf", encoding='ISO-8859-1') as f:
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+        self.aliases = {}
+    def add_edge(self, u, v, w):
+        weight = 1 
+        self.graph[u][v][w] += weight
+    def generate_text(self, start_word, text_length):
+        if start_word not in self.graph:
+            return "Start word not found."
+        current_word = start_word
+        generated_text = [current_word]
+        while len(generated_text) < text_length:
+            next_nodes = self.graph[current_word]
+            if not next_nodes:
+                break
+            next_words = []
+            weights = []
+            total_weights = {}
+            for node, edges in next_nodes.items():
+                total_weight = sum(edges.values())
+                total_weights[node] = total_weight
+            if sum(total_weights.values()) <= 0:
+                break
+            for node, total_weight in total_weights.items():
+                if total_weight > 0:
+                    next_words.append(node)
+                    weights.append(len(next_words) * math.log2(1 / total_weight + 1))
+            next_word = random.choices(next_words, weights=weights, k=1)[0]
+            generated_text.append(next_word)
+            current_word = next_word
+        return ' '.join(generated_text)
+def preprocess_text(text, user_words):
+    sentences = re.split(r'(?<=[.!?])\s+', text.lower())
+    user_words_set = set(user_words)
+    filtered_words = [word for sentence in sentences for word in sentence.split() if user_words_set.intersection(sentence.split())]
+    return filtered_words
+def create_word_graph(text, user_words, n=3):
+    words = text.split()
+    word_graph = Graph()
+    aliases = {}
+    for i in range(len(words) - n + 1):
+        ngram = tuple(words[i:i + n])
+        if ngram in aliases:
+            word_graph.add_edge(*aliases[ngram])
+        else:
+            aliases[ngram] = words[i:i + n]
+            word_graph.add_edge(*ngram)
+    return word_graph
+with open("FileList.conf", encoding="ISO-8859-1") as f:
     files = f.read().splitlines()
-print("SynthReason - Synthetic Dawn")
-with open("questions.conf", encoding='ISO-8859-1') as f:
+with open("questions.conf", encoding="ISO-8859-1") as f:
     questions = f.read().splitlines()
-ngram_size = 3
-while True:
-    user = re.sub('\W+', ' ', input("USER: ").lower())
+filename = "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
+random.shuffle(questions)
+for question in questions:
     random.shuffle(files)
     for file in files:
-        with open(file, encoding='UTF-8') as f:
+        with open(file, encoding="UTF-8") as f:
             text = f.read()
-        sentences = text.split(token)
-        sentences = [sentence for sentence in sentences if any(word in sentence for word in user.split())]
-        random.shuffle(sentences)
-        sine_frequency, cosine_frequency, amplitude, phase = 5.2, 0.5, 0.4, 1.1  # Adjust as needed
-        ngrams = []
-        for sentence in sentences:
-            words = sentence.split()
-            for i in range(len(words) - ngram_size + 1):
-                ngram = words[i:i + ngram_size]
-                if len(ngram) == len(set(ngram)) and i % 3 == 0:  # Check for no repeated words
-                    ngram = " ".join(ngram)
-                    ngrams.append(ngram)
-        sine_values = [ngram.rfind(" ") / math.atan(4 / math.pi / sine_frequency * ngram.find(" ") + phase) for ngram in reversed(ngrams)]
-        degrees_values = [amplitude / math.degrees(2 / math.pi / cosine_frequency * i + phase) for i in sine_values]
-        combined_wave = [s * c for s, c in zip(sine_values, degrees_values)]
-        def custom_sort(item):
-            ngram, wave_value = item
-            return (-wave_value, len(ngram))
-        ngrams_with_wave = sorted(zip(ngrams, combined_wave), key=custom_sort, reverse=True)
-        selected_ngrams = [ngram.split() for ngram, _ in ngrams_with_wave][:size]
-        output_with_wave = ' '.join([' '.join(ngram) for ngram in selected_ngrams])
-        print("\nusing:", file.strip(), "answering:", user, "\nAI:", output_with_wave, "\n\n")
-        filename = "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
-        with open(filename, "a", encoding="utf8") as f:
-            f.write("\nusing: " + file.strip() + " answering: " + user + "\n" + output_with_wave + "\n")
-        break
+        user_input = input("Enter start word: ").strip().lower()
+        if not user_input:
+            continue
+        user_words = re.sub("\W+", " ", user_input).split()
+        filtered_text = ' '.join(preprocess_text(text, user_words))
+        word_graph = create_word_graph(filtered_text, user_input)
+        generated_text = word_graph.generate_text(user_words[-1], size)
+        if generated_text:
+            print("\nUsing:", file.strip(), "Answering:", user_input, "\nAI:", generated_text, "\n\n")
+            with open(filename, "a", encoding="utf8") as f:
+                f.write("\nUsing: " + file.strip() + " Answering: " + user_input + "\n" + generated_text + "\n")
+            break
