ad27a45bcf0c08deab30f042d475bd49f69131bb
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-02-11 13:52:30 +1100

    SynthReason v2.0 *ULTRA*

--- SynthReason.py
+++ SynthReason.py
@@ -1,58 +1,53 @@
-# SynthReason v1.81 *ULTRA*
+# SynthReason v2.0 *ULTRA*
 # Copyright 2024 George Wagenknecht
 import re
 import random
-import math
 from collections import defaultdict
 size = 250
 class Graph:
     def __init__(self):
-        self.graph = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-        self.aliases = {}
-    def add_edge(self, u, v, w):
-        weight = 1 
-        self.graph[u][v][w] += weight
+        self.graph = defaultdict(lambda: defaultdict(int))
+        self.transition_probabilities = defaultdict(lambda: defaultdict(float))
+        self.start_probabilities = defaultdict(float)
+        self.total_starts = 0
+    def add_edge(self, u, v):
+        self.graph[u][v] += 1
+    def calculate_probabilities(self):
+        for u in self.graph.keys():
+            total_outgoing = sum(self.graph[u].values())
+            for v in self.graph[u]:
+                self.transition_probabilities[u][v] = self.graph[u][v] / total_outgoing
+        for start in self.start_probabilities.keys():
+            self.start_probabilities[start] += self.total_starts
+    def add_start(self, start):
+        self.start_probabilities[start] += 1
+        self.total_starts += 1
     def generate_text(self, start_word, text_length):
-        if start_word not in self.graph:
-            return "Word not found."
+        if start_word not in self.transition_probabilities:
+            return "Word not found."  
         current_word = start_word
         generated_text = [current_word]
         while len(generated_text) < text_length:
-            next_nodes = self.graph[current_word]
-            if not next_nodes:
+            probabilities = list(self.transition_probabilities[current_word].items())
+            if not probabilities:
                 break
-            next_words = []
-            weights = []
-            total_weights = {}
-            for node, edges in next_nodes.items():
-                total_weight = sum(edges.values())
-                total_weights[node] = (1/total_weight)
-            if sum(total_weights.values()) <= 0:
-                break
-            for node, total_weight in total_weights.items():
-                if total_weight > 0:
-                    next_words.append(node)
-                    weights.append(total_weight * math.log2(1 / len(node) + 1))
-            next_word = random.choices(next_words, weights=weights, k=1)[0]
+            next_words, probs = zip(*probabilities)
+            next_word = random.choices(next_words, weights=probs, k=1)[0]
             generated_text.append(next_word)
             current_word = next_word
         return ' '.join(generated_text)
-def preprocess_text(text, user_words):
+def preprocess_text(text):
     sentences = re.split(r'(?<=[.!?])\s+', text.lower())
-    user_words_set = set(user_words)
-    filtered_words = [word for sentence in sentences for word in sentence.split() if user_words_set.intersection(sentence.split())]
-    return filtered_words
-def create_word_graph(text, user_words, n=3):
-    words = text.split()
+    return [word for sentence in sentences for word in sentence.split()]
+def create_word_graph(text, n=3):
+    words = preprocess_text(text)
     word_graph = Graph()
-    aliases = {}
-    for i in range(len(words) - n + 1):
-        ngram = tuple(words[i:i + n])
-        if ngram in aliases:
-            word_graph.add_edge(*aliases[ngram])
-        else:
-            aliases[ngram] = words[i:i + n]
-            word_graph.add_edge(*ngram)
+    for i in range(len(words)-1):
+        start, next_word = words[i], words[i + 1]
+        word_graph.add_edge(start, next_word)
+        if i == 0 or words[i-1] in '.!?':
+            word_graph.add_start(start)
+    word_graph.calculate_probabilities()
     return word_graph
 with open("FileList.conf", encoding="ISO-8859-1") as f:
     files = f.read().splitlines()
@@ -69,7 +64,7 @@ for question in questions:
         if not user_input:
             continue
         user_words = re.sub("\W+", " ", user_input).split()
-        filtered_text = ' '.join(preprocess_text(text, user_words))
+        filtered_text = ' '.join(preprocess_text(text))
         word_graph = create_word_graph(filtered_text, user_input)
         generated_text = word_graph.generate_text(user_words[-1], size)
         if generated_text:
