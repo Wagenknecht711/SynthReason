8ae075d42b91a34e526fe8eda3f4b60d45f6dfa9
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-02-12 21:52:52 +1100

    SynthReason v5.4 *ULTRA*

--- SynthReason - auto.py
+++ SynthReason - auto.py
@@ -1,27 +1,33 @@
-# SynthReason v5.3 *ULTRA*
+# SynthReason v5.4 *ULTRA*
 # Copyright 2024 George Wagenknecht
 import re
 import random
 from collections import defaultdict
+
 size = 250
+
 class Graph:
     def __init__(self):
         self.graph = defaultdict(lambda: defaultdict(int))
         self.transition_probabilities = defaultdict(lambda: defaultdict(float))
         self.start_probabilities = defaultdict(float)
         self.total_starts = 0
+
     def add_edge(self, u, v):
         self.graph[u][v] += 1
+
     def calculate_probabilities(self):
         for u in self.graph.keys():
             total_outgoing = sum(self.graph[u].values())
             for v in self.graph[u]:
                 self.transition_probabilities[u][v] = self.graph[u][v] / total_outgoing
-                for start in self.start_probabilities.keys():
-                    self.transition_probabilities[u][v] += self.total_starts
+        for start in self.start_probabilities.keys():
+            self.start_probabilities[start] /= self.total_starts
+
     def add_start(self, start):
         self.start_probabilities[start] += 1
         self.total_starts += 1
+
     def generate_text(self, start_word, text_length):
         if start_word not in self.transition_probabilities:
             return "Word not found."  
@@ -36,27 +42,33 @@ class Graph:
             generated_text.append(next_word)
             current_word = next_word
         return ' '.join(generated_text)
+
 def preprocess_text(text, user_words):
     sentences = re.split(r'(?<=[.!?])\s+', text.lower())
     user_words_set = set(user_words)
     filtered_words = [word for sentence in sentences for word in sentence.split() if set(sentence.split()).intersection(user_words_set)]
     return filtered_words
-def create_word_graph(text, n=3):
+
+def create_word_graph(text):
     words = text.split()
     word_graph = Graph()
     for i in range(len(words)-5):
         start, next_word = words[i+4], words[i + 5]
         word_graph.add_edge(start, next_word)
         if i == 0 or words[i-1] in '.!?':
-            word_graph.add_start(words[i])
+            word_graph.add_start(start)
     word_graph.calculate_probabilities()
     return word_graph
+
 with open("FileList.conf", encoding="ISO-8859-1") as f:
     files = f.read().splitlines()
+
 with open("questions.conf", encoding="ISO-8859-1") as f:
     questions = f.read().splitlines()
+
 filename = "Compendium#" + str(random.randint(0, 10000000)) + ".txt"
 random.shuffle(questions)
+
 for question in questions:
     random.shuffle(files)
     for file in files:
@@ -67,7 +79,7 @@ for question in questions:
             continue
         user_words = re.sub("\W+", " ", user_input).split()
         filtered_text = ' '.join(preprocess_text(text,user_words))
-        word_graph = create_word_graph(filtered_text, questions)
+        word_graph = create_word_graph(filtered_text)
         generated_text = word_graph.generate_text(user_words[-1], size)
         if generated_text:
             print("\nUsing:", file.strip(), "Answering:", user_input, "\nAI:", generated_text, "\n\n")
