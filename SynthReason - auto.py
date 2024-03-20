1e93a6b3e630f2f840b9fbf0a912095afc01652d
Wagenknecht711 <155693723+Wagenknecht711@users.noreply.github.com>
2024-03-20 16:55:38 +1100

    SynthReason v14.6 *ULTRA*

--- SynthReason - auto.py
+++ SynthReason - auto.py
@@ -1,57 +1,54 @@
-# SynthReason v16.7 *EPIC*
+# SynthReason v14.6 *ULTRA*
 # Copyright 2024 George Wagenknecht
+import re
 import random
+import numpy as np
 import math
-size = 50
+size = 250
 n = 3
-n2 = 16
-precision = 2
-def fit(text, n):
+memoryLimiter = 50000
+def fit(text):
     words = text.lower().split()
     unique_words = list(set(words))
     num_states = len(unique_words)
-    ngrams = [" ".join(words[i:i+n]) for i in range(len(words)-n+1)]
-    transitions = [[0] * num_states for _ in range(num_states)]
-    for i in range(len(transitions) - 1):
-        keyword_frequencies = {keyword: ngrams.count(keyword) for keyword in set(ngrams)}
-        spatial_frequency_range = [keyword_frequencies[ngrams[j]] for j in range(len(ngrams))]
-        for e in spatial_frequency_range:
-            u, v = unique_words.index(words[i-1]), unique_words.index(words[i])
-            if u > 1 and v > 1:
-                transitions[u][v] += e
-        for j, u in enumerate(spatial_frequency_range):
-            transitions[-1][0] *= 1
+    transitions = np.zeros((num_states, num_states))
+    keyword_frequencies = {keyword: words.index(keyword) for keyword in unique_words}
+    spatial_frequency_range = np.array([keyword_frequencies[keyword] for keyword in unique_words])
+    n = 0
+    for i in spatial_frequency_range:
+        u, v = unique_words.index(words[n]), unique_words.index(words[n + 1])
+        if u > 1 and v > 1:
+            transitions[u][v] += n
+            n+=1
+    transitions *= np.array([i for i in list(reversed(spatial_frequency_range))])
+    row_sums = transitions.sum(axis=1, keepdims=True)
+    transitions = np.where(row_sums != 0, transitions / row_sums, transitions)
     for i in range(num_states):
-        row_sum = sum(transitions[i])
-        if row_sum == 0:
-            transitions[i] = [1 / num_states] * num_states
-        else:
-            transitions[i] = [transitions[i][j] / row_sum for j in range(num_states)]
-    return transitions, unique_words, ngrams
-def generate_text(transitions, unique_words, ngrams, start_ngram, text_length, n):
-    if start_ngram not in unique_words:
-        return "N-gram not found."
-    generated_text = start_ngram.split()
-    current_state = unique_words.index(start_ngram)
+        if row_sums[i] == 0:
+            transitions[i] = np.ones(num_states) / num_states
+    return transitions, unique_words
+def generate_text(transitions, unique_words, start_word, text_length, n):
+    if start_word not in unique_words:
+        return "Word not found."
+    generated_text = [start_word]
+    current_state = unique_words.index(start_word)
     for _ in range(text_length - n):
-        next_states = list(range(len(transitions[current_state])))
+        next_states = np.arange(len(transitions[current_state]))
         probabilities = transitions[current_state]
-        next_state = random.choices(next_states, weights=probabilities)[0]
-        if next_state < len(ngrams):
-            next_ngram = ngrams[next_state]
-            next_word = next_ngram.split()[-1]
-        if len(''.join(filter(str.isalnum, next_word))) > len(next_word) - 2 and next_ngram not in generated_text:
-            generated_text.append(next_ngram)
-        current_state = unique_words.index(next_word)
+        next_state = np.random.choice(next_states, p=probabilities)
+        next_word = unique_words[next_state]
+        if len(re.sub(r'[^a-zA-Z0-9\s]', '', next_word)) >len(next_word)-2:
+            generated_text.append(next_word)
+        current_state = next_state
+        if current_state ==  next_state+4:
+            current_state = len(unique_words) - 1
+        elif current_state ==  _ - 1:
+            next_state+4
     return ' '.join(generated_text)
 def preprocess_text(text, user_words):
-    sentences = text.lower().split('.')
+    sentences = re.split(r'(?<=[.!?])\s+', text.lower())
     user_words_set = set(user_words)
-    filtered_words = []
-    for sentence in sentences:
-        if len(set(sentence.split()).intersection(user_words_set)) > precision:
-            filtered_words.append(sentence)
-    return filtered_words
+    return [word for sentence in sentences for word in sentence.split() if len(set(sentence.split()).intersection(user_words_set))>1]
 with open("FileList.conf", encoding="ISO-8859-1") as f:
     files = f.read().splitlines()
 with open("questions.conf", encoding="ISO-8859-1") as f:
@@ -61,14 +58,14 @@ random_number = random.randint(0, 10000000)
 filename = "Compendium#" + str(random_number) + ".txt"
 for question in questions:
     random.shuffle(files)
-    user_input = question.strip().lower()
+    user_input = question.strip().lower() 
     for file in files:
         with open(file, encoding="UTF-8") as f:
             text = f.read() 
-        user_words = ''.join(e if e.isalnum() or e.isspace() else ' ' for e in user_input).split()
-        filtered_text = ' '.join(preprocess_text(text, user_words))
-        transitions, unique_words, ngrams = fit(filtered_text,n2)
-        generated_text = generate_text(transitions, unique_words, ngrams, user_words[-1], size, n)
+        user_words = re.sub("\W+", " ", user_input).split()
+        filtered_text = ' '.join(preprocess_text(text, user_words)[:memoryLimiter])
+        transitions, unique_words = fit(filtered_text)
+        generated_text = generate_text(transitions, unique_words, user_words[-1], size, n)
         if len(generated_text) > len("Word not found."):
             print("\nUsing:", file.strip(), "Answering:", user_input, "\nAI:", generated_text, "\n\n")
             with open(filename, "a", encoding="utf8") as f:
